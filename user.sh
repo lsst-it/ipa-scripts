69b47d1e2623cfe6462c25c5694bf69b10274038
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2023-05-10 20:25:19 -0400

    removed trailing white space

--- user.sh
+++ user.sh
@@ -2,9 +2,8 @@
 users=$(cat /home/dtapia/dryrun/disabled_users.json | jq -r '.[].uid[]')
 for user in "${users[@]}"
 do
-    echo "Running command for user: $user"
-    ipa user-del --preserve --dry-run $user
-    echo "User $user was deleted"
+echo "Running command for user: $user"
+ipa user-del --preserve --dry-run $user
+echo "User $user was deleted"
 done
 echo "All users have been deleted"
-
